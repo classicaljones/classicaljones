658e5491520a607b1feb728decc8af71ee9c60b1
francis <franciskwadwofordjour@gmail.com>
2024-11-28 03:15:44 +0000

    commit

--- products/views.py
+++ products/views.py
@@ -57,15 +57,19 @@ def cart(request):
 
 @login_required(login_url='/users/login/')
 def checkout(request):
+    
     if request.user.is_authenticated:
         customer = request.user.customer
         order,created = Order.objects.get_or_create(customer=customer,complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
+
     else:
         items = []
         order = {'get_cart_total': 0,'get_cart_items': 0,'shipping':False}
         cartItems = order['get_cart_items']
+
+    # receiving checkout form
     
     context = {'items': items, 'order':order,'cartItems':cartItems}
     return render(request,'product_page/checkout.html',context)
